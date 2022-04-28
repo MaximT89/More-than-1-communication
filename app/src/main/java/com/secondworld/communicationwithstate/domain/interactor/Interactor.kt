002f575cb447daf76ba5a69a638ca7008365e1d0
Максим Толстобров <maximah1989@yandex.ru>
2022-04-28 10:01:51 +0300

    Refactoring

--- app/src/main/java/com/secondworld/communicationwithstate/domain/interactor/Interactor.kt
+++ app/src/main/java/com/secondworld/communicationwithstate/domain/interactor/Interactor.kt
@@ -10,18 +10,18 @@ interface Interactor<T> {
     fun get() : T
 
     class InteractorProductManager @Inject constructor(private val repository: Repository) : Interactor<Users.ProductManager>{
-        override fun get(): Users.ProductManager = repository.fetchProductManager()
+        override fun get() = repository.fetchProductManager()
     }
 
     class InteractorManager @Inject constructor(private val repository: Repository) : Interactor<Users.Manager>{
-        override fun get(): Users.Manager = repository.fetchManager()
+        override fun get() = repository.fetchManager()
     }
 
     class InteractorDesigner @Inject constructor(private val repository: Repository) : Interactor<Users.Designer>{
-        override fun get(): Users.Designer = repository.fetchDesigner()
+        override fun get() = repository.fetchDesigner()
     }
 
     class InteractorAdmin @Inject constructor(private val repository: Repository) : Interactor<Users.Admin>{
-        override fun get(): Users.Admin = repository.fetchAdmin()
+        override fun get()= repository.fetchAdmin()
     }
 }
